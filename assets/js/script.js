c58d9c9f8449b6a2432d25c0320d85220b32377d
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-02-13 12:05:00 +0000

    add variable to check if chosen letter occurs or not

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,37 @@
 const lettersDisplay = document.querySelector('.letters-display');
 const alphabetDiv = document.querySelector('.alphabet');
 
+let presentWord;
+
 const getRandomWord = () => {
     //Select random word and clue from wordList
     const { word, clue } = wordList [Math.floor(Math.random() * wordList.length )];
+    presentWord = word;
+
     console.log(word);
     document.querySelector('.clue-text b').innerText = clue;
     lettersDisplay.innerHTML = word.split('').map(()=>`<li class="letters"></li>`).join('');
-
 } 
 
-//Create the alphabet buttons
+const initGame = (button, chosenLetter) => {
+    //Check if the chosen letter occurs in the word
+    if(presentWord.includes(chosenLetter)) {
+        console.log (chosenLetter, 'Occurs in the word');
+    } else {
+        console.log (chosenLetter, 'Does not occur in the word');
+    }
+
+
+    console.log(button, chosenLetter);
+
+}
+
+//Create the alphabet buttons and add eventListeners
 for (let i = 97; i < 123; i++){
     const button = document.createElement('button');
     button.innerText = String.fromCharCode(i);
     alphabetDiv.appendChild(button);
+    button.addEventListener('click', e => initGame(e.target, String.fromCharCode(i)));
     
 }
 
